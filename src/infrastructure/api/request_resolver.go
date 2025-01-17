683cecbdd687e6db0bda92bac86644f6cadf4e5b
simon <simon.garcial@udea.edu.co>
2025-01-17 17:47:12 -0500

    :sparkles: request resolver

--- src/infrastructure/api/request_resolver.go
+++ src/infrastructure/api/request_resolver.go
@@ -1,1 +1,55 @@
 package api
+
+import (
+	"gingoskeleton/src/application/shared/status"
+	usecase "gingoskeleton/src/application/shared/use_case"
+
+	gin "github.com/gin-gonic/gin"
+)
+
+type RequestResolver struct {
+	statusMapping map[status.ApplicationStatusEnum]int
+}
+
+func NewRequestResolver() *RequestResolver {
+	return &RequestResolver{
+		statusMapping: map[status.ApplicationStatusEnum]int{
+			status.Success:                   200,
+			status.Updated:                   200,
+			status.Created:                   201,
+			status.PartialContent:            206,
+			status.InvalidInput:              400,
+			status.Unauthorized:              401,
+			status.NotFound:                  404,
+			status.Conflict:                  409,
+			status.InternalError:             500,
+			status.NotImplemented:            501,
+			status.ProviderError:             502,
+			status.ChatProviderError:         502,
+			status.ProviderEmptyResponse:     502,
+			status.ProviderEmptyCacheContext: 502,
+		},
+	}
+}
+
+func (rr *RequestResolver) ResolveDTO(
+	ctx *gin.Context,
+	result usecase.UseCaseResult[any],
+	headersToAdd map[HTTPHeaderTypeEnum]string,
+) (*gin.H, int) {
+	content := gin.H{}
+
+	content["data"] = result.Data
+	content["details"] = result.Details
+	rr.getHeaders(ctx, headersToAdd)
+
+	return &content, rr.statusMapping[result.StatusCode]
+}
+
+func (rr *RequestResolver) getHeaders(
+	ctx *gin.Context, headersToAdd map[HTTPHeaderTypeEnum]string,
+) {
+	for key, value := range headersToAdd {
+		ctx.Header(key.String(), value)
+	}
+}
